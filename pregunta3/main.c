b04c8d9f4bea60d2cf0f446b3e29d26584ee0b76
RobertoCastroB <46934782+RobertoCastroB@users.noreply.github.com>
2022-03-28 11:49:30 -0600

    Update main.c

--- pregunta3/main.c
+++ pregunta3/main.c
@@ -2,6 +2,7 @@
 #include "rot13.h"
 
 // 1. Proporciona los comandos para compilar un ejecutable usando la librería estática.
+// R= $gcc -static main.c -L. -lroot13 -o main
 int main() {
 	char p[] = "Hola Mundo Este String Se Codifica Y Decodifica";
 
