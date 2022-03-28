a2a29d46f8508c3e6985e10cd221d886a71851dd
RobertoCastroB <46934782+RobertoCastroB@users.noreply.github.com>
2022-03-28 11:50:30 -0600

    Update mystery.c

--- pregunta1/mystery.c
+++ pregunta1/mystery.c
@@ -11,8 +11,9 @@ int main() {
     // ¿Por qué se imprimen alternados con 0 los numeros asignados en el for anterior?
     // R= Debido al manejo de bytes, short contiene solo 2 bytes por lo que se imprime un segmento. Podemos ver que los
     // valores cambian si aumentamos el rango de i.
-    for(i=0; i<10; i++) {
+    for(i=0; i<30; i++) {
         printf("%d\n", *I2S(x+i));
+        printf("direccion de x + i : %p\n", I2S(x+i));
     }
     
 }
